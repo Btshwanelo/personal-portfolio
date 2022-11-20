c592afe43ab53133d29491e483e8d0727334c69a
Btshwanelo <48760912+Btshwanelo@users.noreply.github.com>
2022-11-20 23:28:49 +0200

    Update projects.js
    - added live link for personal portfolio and music-app-clone

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -1,45 +1,46 @@
 export const projects = [
   {
-    category: "React Js",
-    name: "starwars api",
-    img: "/images/star-wars.png",
-    github: "https://github.com/Btshwanelo/starwars_api",
-    id: "1"
+    category: 'React Js',
+    name: 'starwars api',
+    img: '/images/star-wars.png',
+    github: 'https://github.com/Btshwanelo/starwars_api',
+    id: '1',
   },
   {
-    category: "React Js",
-    name: "shopping cart",
-    img: "/images/shopping_cart.png",
-    github: "https://github.com/Btshwanelo/react-shopping-cart-template",
-    id: "2"
+    category: 'React Js',
+    name: 'shopping cart',
+    img: '/images/shopping_cart.png',
+    github: 'https://github.com/Btshwanelo/react-shopping-cart-template',
+    id: '2',
   },
   {
-    category: "React Js",
-    name: "personal portfolio",
-    img: "/images/portfolio.jpg",
-    github: "https://github.com/Btshwanelo/personal-portfolio",
-    id: "3"
+    category: 'React Js',
+    name: 'personal portfolio',
+    img: '/images/portfolio.jpg',
+    github: 'https://github.com/Btshwanelo/personal-portfolio',
+    live: 'https://tshwanelo-portfolio.netlify.app',
+    id: '3',
   },
   {
-    category: "React Js",
-    name: "music app",
-    img: "/images/music-app.jpg",
-    github: "https://github.com/Btshwanelo/music-app",
-    id: "4"
+    category: 'React Js',
+    name: 'music app',
+    img: '/images/music-app.jpg',
+    github: 'https://github.com/Btshwanelo/music-app',
+    live: 'https://music-cloned-app.netlify.app',
+    id: '4',
   },
   {
-    category: "PWA",
-    name: "pokemon pwa app",
-    img: "/images/pokemon.jpeg",
-    github: "https://github.com/Btshwanelo/pokemon-pwa-application",
-    id: "5"
+    category: 'PWA',
+    name: 'pokemon pwa app',
+    img: '/images/pokemon.jpeg',
+    github: 'https://github.com/Btshwanelo/pokemon-pwa-application',
+    id: '5',
   },
   {
-    category: "PWA",
-    name: "weather pwa app",
-    img: "/images/weather.png",
-    github: "https://github.com/Btshwanelo/weather-app",
-    id: "6"
+    category: 'PWA',
+    name: 'weather pwa app',
+    img: '/images/weather.png',
+    github: 'https://github.com/Btshwanelo/weather-app',
+    id: '6',
   },
 ];
-
