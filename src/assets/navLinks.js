44a1ab7778ee9f8c08368cf211faa21432e0e7c3
Btshwanelo <48760912+Btshwanelo@users.noreply.github.com>
2023-03-21 20:03:06 +0200

    removed all code.

--- src/assets/navLinks.js
+++
@@ -1,7 +0,0 @@
-export const navLinks = [
-  { navLinkId: "Home", scrollToId: "homeContainer", id: "1" },
-  { navLinkId: "Portfolio", scrollToId: "portfolioContainer", id: "2" },
-  { navLinkId: "Resume", scrollToId: "resumeContainer", id: "3" },
-  { navLinkId: "About", scrollToId: "aboutContainer", id: "4" },
-  { navLinkId: "Contact", scrollToId: "contactContainer", id: "5" },
-];
--- src/components/index.js
+++
@@ -1,8 +0,0 @@
-export { default as About } from "./About";
-export { default as Contact } from "./Contact";
-export { default as HeroSection } from "./HeroSection";
-export { default as Navbar } from "./Navbar";
-export { default as Portfolio } from "./Portfolio";
-export { default as Resume } from "./Resume";
-export { default as Footer } from "./Footer";
-export { default as TechStack } from "./TechStack";
